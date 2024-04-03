7ebeefefb9fd988e066fb398df60f60290a9c9d6
sbottingota <saku.bottingota@gmail.com>
2024-04-03 12:55:02 +0100

    Added improvements and fixed broken win checking

--- run.sh
+++ run.sh
@@ -3,26 +3,35 @@
 declare -r BOARD_SIZE=9
 declare -r DEFAULT_SQUARE_VALUE=-
 declare -ra PLAYERS=(O X)
+declare -ra WINNING_COMBINATIONS=("1 2 3" "4 5 6" "7 8 9" "1 4 7" "2 5 8" "3 6 9" "1 5 9" "3 5 7")
+
+print_move_indices() {
+    echo "Move indices:"
+    echo "1|2|3"
+    echo "4|5|6"
+    echo "7|8|9"
+    echo
+}
 
 draw_board() {
+    shift
     for i in {1..3}
     do
-        echo "${1:- }|${2:- }|${3:- }"
+        echo "$1|$2|$3"
         shift 3
     done
 }
 
 
 get_placement() {
-    while :
+shift
+    while true
     do
-        echo "Please enter a valid move (0-$((BOARD_SIZE - 1)))"
-        read n
+        echo "Please enter a valid move (1-$BOARD_SIZE)"
+        read -r n
         echo
 
-        digit_pattern="^[0-$((BOARD_SIZE-1))]\$"
-        args=("$@")
-        if [[ $n =~ $digit_pattern ]] && [[ ${args[$n]} == $DEFAULT_SQUARE_VALUE ]]
+        if [[ "$n" -gt 0 ]] && [[ "$n" -le "$BOARD_SIZE" ]] && [[ "${!n}" == "$DEFAULT_SQUARE_VALUE" ]]
         then
             placement=$n
             return
@@ -31,103 +40,71 @@ get_placement() {
     done
 }
 
-# sets $ended to whether the game has ended and $winner to the winner
-get_winner() {
-    ended=1
-    possible_draw=1
-
-    # check for draw
-    for square in $@
+is_draw() {
+    shift
+    for square in "$@"
     do
-        if [[ $square == $DEFAULT_SQUARE_VALUE ]]
+        if [[ "$square" == "$DEFAULT_SQUARE_VALUE" ]]
         then
-            possible_draw=0
-            break
+            return 1
+        fi
+    done
+    return 0
+}
+
+is_winner() {
+    shift
+
+    for combination in "${WINNING_COMBINATIONS[@]}"
+    do
+        is_win=1
+        first_square="${combination:0:1}"
+        for square in $combination
+        do
+            if [[ "${!square}" == "$DEFAULT_SQUARE_VALUE" ]] || [[ "${!square}" != "${!first_square}" ]]
+            then
+                is_win=0
+                break
+            fi
+        done
+
+        if [[ $is_win -eq 1 ]]
+        then
+            return 0
         fi
     done
 
-    # check for winners
-    if [[ ${PLAYERS[*]} =~ "$1" ]] && [[ ${PLAYERS[*]} =~ "$5" ]] && [[ ${PLAYERS[*]} =~ "$9" ]]
-    then
-        if [[ "$1" == "$5"  ]] && [[ "$5" == "$9" ]]
-        then
-            winner=$1
-            return
-        fi
-    fi
-
-    if [[ ${PLAYERS[*]} =~ "$3" ]] && [[ ${PLAYERS[*]} =~ "$5" ]] && [[ ${PLAYERS[*]} =~ "$7" ]]
-    then
-        if [[ "$3" == "$5"  ]] && [[ "$5" == "$7" ]]
-        then
-            winner=$3
-            return
-        fi
-    fi
-
-    for i in {1..3}
-    do
-        if [[ ${PLAYERS[*]} =~ "$1" ]] && [[ ${PLAYERS[*]} =~ "$2" ]] && [[ ${PLAYERS[*]} =~ "$3" ]]
-        then
-            if [[ "$1" == "$2"  ]] && [[ "$2" == "$3" ]]
-            then
-                winner=$1
-                return
-            fi
-        fi
-
-        if [[ ${PLAYERS[*]} =~ "$1" ]] && [[ ${PLAYERS[*]} =~ "$4" ]] && [[ ${PLAYERS[*]} =~ "$7" ]]
-        then
-            if [[ "$1" == "$4"  ]] && [[ "$4" == "$7" ]]
-            then
-                winner=$1
-                return
-            fi
-        fi
-
-        shift 3
-    done
-
-    winner=''
-
-    if [[ $possible_draw == 1 ]]
-    then
-        return
-    fi
-
-    ended=0
+    return 1
 
 } 
 
-board=( $(for i in $(seq 1 $BOARD_SIZE); do echo "$DEFAULT_SQUARE_VALUE"; done) )
+board=( "/" $(for i in $(seq 1 $BOARD_SIZE); do echo "$DEFAULT_SQUARE_VALUE"; done) ) # dummy value at index 0 so that indices start from 1
 current_turn="O"
 
-while :
+print_move_indices
+
+while true
 do
-    draw_board ${board[@]}
-    get_placement ${board[@]}
-    board[$placement]=$current_turn
-    get_winner ${board[@]}    
+    echo -e "$current_turn's turn\n"
 
-    if [[ "$ended" == 1 ]]
+    draw_board "${board[@]}"
+    get_placement "${board[@]}"
+    board[placement]="$current_turn"
+
+    if is_draw "${board[@]}"
     then
-        if [ -n "$winner" ]
-        then
-            echo "$winner has won!"
-            echo
-            draw_board ${board[@]}
-        else
-            echo "Draw!"
-            echo
-            draw_board ${board[@]}
-        fi
-
+        echo "Draw!"
+        echo
+        draw_board "${board[@]}"
+        exit
+    elif is_winner "${board[@]}"
+    then
+        echo "$current_turn is the winner!"
+        draw_board "${board[@]}"
         exit
     fi
 
-    [[ $current_turn == ${PLAYERS[0]} ]] && current_turn=${PLAYERS[1]} || current_turn=${PLAYERS[0]}
+    [[ "$current_turn" == "${PLAYERS[0]}" ]] && current_turn=${PLAYERS[1]} || current_turn=${PLAYERS[0]}
 
     echo -en "\n\n\n"
 done
-
-
